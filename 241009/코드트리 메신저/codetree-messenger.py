86c59fe7e1d0518923f81adec5275fd084c8bb44
ChangHwan Sung <82512631+sung0503@users.noreply.github.com>
2024-10-09 13:04:01 +0900

    Title: 코드트리 메신저, Time: 8096ms, Memory: 49MB, Status: Time Limit Exceed - CodeTree

--- 241009/코드트리 메신저/codetree-messenger.py
+++ 241009/코드트리 메신저/codetree-messenger.py
@@ -1,94 +1,74 @@
-class Chat():
-    def __init__(self, ci, pi, a):
-        self.ci = ci
-        self.pi = pi
-        self.alarm = True # if false, don't propagate alarm to parent
-        self.authority = a
-        self.left = None
-        self.right = None
-    
-    def reverse_alarm(self):
-        self.alarm = not self.alarm
-    
-    def set_authority(self, power):
-        self.authority = power
+import copy
 
 
 class Chat_Tree():
-    def __init__(self, n, pi_arr, au_arr):
-        self.chat_list = [Chat(ci, 0, 0) for ci in range(n + 1)]
-        for i, (pi, au) in enumerate(zip(pi_arr, au_arr)):
-            ci = i + 1
-            parent = self.chat_list[pi]
-            self.chat_list[ci].pi = pi
-            self.chat_list[ci].authority = au
-            if parent.left is None:
-                parent.left = ci
-            elif parent.right is None:
-                parent.right = ci
+    def __init__(self, n, data):
+        self.pi_list = [0] + [data[1 + i] for i in range(n)]
+        self.auth_list = [0] + [data[1 + n + i] for i in range(n)]
+        self.alarm = [True] * (n + 1)
+        self.children = [[None, None] for _ in range(n + 1)]
+        for ci in range(1, n + 1):
+            pi = self.pi_list[ci]
+            if self.children[pi][0] is None:
+                self.children[pi][0] = ci
+            elif self.children[pi][1] is None:
+                self.children[pi][1] = ci
             else:
                 raise Exception("longer than 2")
 
     def change_alarm(self, ci):
-        self.chat_list[ci].reverse_alarm()
+        self.alarm[ci] = not self.alarm[ci]
 
     def change_authority(self, ci, power):
-        self.chat_list[ci].set_authority(power)
-
-    def get_pi(self, ci):
-        return self.chat_list[ci].pi
+        self.auth_list[ci] = power
 
     def change_parent(self, ci1, ci2):
-        pi1 = self.chat_list[ci1].pi
-        pi2 = self.chat_list[ci2].pi
+        pi1 = self.pi_list[ci1]
+        pi2 = self.pi_list[ci2]
         # change parent id
-        self.chat_list[ci1].pi = pi2
-        self.chat_list[ci2].pi = pi1
+        self.pi_list[ci1] = pi2
+        self.pi_list[ci2] = pi1
         # next, change parent's child (relink)
-        if self.chat_list[pi1].left == ci1:
-            self.chat_list[pi1].left = ci2
+        if self.children[pi1][0] == ci1:
+            self.children[pi1][0] = ci2
         else:
-            self.chat_list[pi1].right = ci2
-        if self.chat_list[pi2].left == ci2:
-            self.chat_list[pi2].left = ci1
+            self.children[pi1][1] = ci2
+        if self.children[pi2][0] == ci2:
+            self.children[pi2][0] = ci1
         else:
-            self.chat_list[pi2].right = ci1
+            self.children[pi2][1] = ci1
 
     def count_alarm_reachable(self, ci):
-        chat = self.chat_list[ci]
         count = 0
-        if chat.left is not None:
-            count += self.dfs(chat.left, 1)
-        if chat.right is not None:
-            count += self.dfs(chat.right, 1)
+        for child in self.children[ci]:
+            if child is not None:
+                count += self.dfs(child, 1)
         print(count)
 
     def dfs(self, ci, threshold):
         # check this chat can propagate alarm to parent
-        chat = self.chat_list[ci]
-        if not chat.alarm:
+        if not self.alarm[ci]:
             return 0
         # count chat which can reach alarm to ci
-        if chat.authority < threshold:
+        if self.auth_list[ci] < threshold:
             count = 0
         else:
             count = 1
-        if chat.left is not None:
-            count += self.dfs(chat.left, threshold + 1)
-        if chat.right is not None:
-            count += self.dfs(chat.right, threshold + 1)
+        for child in self.children[ci]:
+            if child is not None:
+                count += self.dfs(child, threshold + 1)
         return count
 
     # def print_tree(self):
-    #     for chat in self.chat_list:
-    #         print(f"pi:{chat.pi} ci:{chat.ci} al:{chat.alarm} au:{chat.authority} l:{chat.left} r:{chat.right}")
+    #     for i in range(len(self.pi_list)):
+    #         print(f"ci:{i} pi:{self.pi_list[i]} al:{self.alarm[i]} au:{self.auth_list[i]} c:{self.children[i]}")
 
 
 def main():
     N, Q = list(map(int, input().split()))
     data = list(map(int, input().split()))
     assert data[0] == 100 and len(data) == 2 * N + 1
-    tree = Chat_Tree(N, data[1: N + 1], data[N + 1:])
+    tree = Chat_Tree(N, data)
     for i in range(Q - 1):
         data = list(map(int, input().split()))
         if data[0] == 200:
